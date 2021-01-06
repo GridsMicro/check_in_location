620ce0ee63eb2201f77dfbb842ac31af07175c28
GridsMicro <69285464+GridsMicro@users.noreply.github.com>
2021-01-06 13:39:40 +0700

    Update getlocal.php

--- AdminLTE/getlocal.php
+++ AdminLTE/getlocal.php
@@ -1,9 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>DH.Workflow | Dashboard</title>
+
+    <script type="text/javascript">
+      var currenttime = '<?php echo date('F j, Y H:i:s'); ?>';
+      var d=new Date();
+
+      function displaytime()
+      {
+        d.setSeconds(d.getSeconds()+1);
+        var s=d.getSeconds();
+        var m=d.getMinutes();
+        var h=d.getHours();
+        var day=d.getDay();
+        var date=d.getDate();
+        var month=d.getMonth();
+        var year=d.getFullYear();
+        var days=new Array("อาทิตย์","จันทร์","อังคาร","พุธ","พฤหัส","ศุกร์","เสาร์");
+        var months=new Array('มกราคา','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม');
+        var date;
+        var time;
+        if (s<10) {s="0" + s}
+        if (m<10) {m="0" + m}
+        //if (h>12) {h-=12;am_pm = "pm"}
+        //else {am_pm="am"}
+        if (h<10) {h="0" + h}
+        date = "วัน" + days[day] + " ที่ " + date + " " + months[month] + " " + year;
+        time = "เวลา " + h + ":" + m + ":" + s;
+        document.getElementById("clock").innerHTML = date + time;
+        setTimeout(displaytime,1000);
+      }
+
+      window.onload = function()
+      {
+        displaytime();
+      }
+    </script>
 
     <!-- Google Font: Source Sans Pro -->
     <link
@@ -928,9 +964,17 @@
         <div class="container">
           <div class="row">
             <div class="col col-sm-12">
-              <h3  class="jumbotron" align="center">DH Check In Location - bete</h3>
+              <h3  class="jumbotron" align="center">Check In Time&Location</h3>
             </div>
           </div>
+
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Note:</strong>The geolocation property is not supported in IE8 and earlier versions.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
         </div>
         <!-- /.content-header -->
 
@@ -946,14 +990,19 @@
                   <button type="button" class="btn btn-primary" onclick="showPosition();">CHECK IN</button>
                 </div>
 
-                <p><strong>Note:</strong> The geolocation property is not supported in IE8 and earlier versions.</p> 
-
                 <p></p>
+
+                <div class="text-center">
+                  <h5 class="heading text-info">                  
+                    <div id="clock">กำลังโหลด...</div>                                
+                  </h5>
+                </div>  
+
                 <!-- onClick.Action embedded in <div> -->
-                <div id="embedMap" style="width: 325px; height: 300px;">
+                <div id="embedMap" style="width: 325px; height: 280px;">
                 <!-- Google map will be embedded here -->
-                </div>              
-                
+                </div>
+                             
               </div>
             </div>
           </div>
@@ -972,7 +1021,7 @@
       <footer class="main-footer">
         <strong
           >Copyright &copy; 2020-2021
-          <a href="https://app.microtronic.biz">DOMNICK WORKFLOW BETA</a>.</strong
+          <a href="https://app.microtronic.biz">CHECK IN LOCATION BETA</a>.</strong
         >
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
@@ -1068,3 +1117,4 @@
     </script>
   </body>
 </html>
+
